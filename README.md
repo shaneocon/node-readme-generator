0044c84fa0075518be07cf494f339dcb62479d19
shaneocon <shane.pat.oconnell@gmail.com>
2021-01-07 17:21:24 -0800

    added const

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # node-readme-generator
 
-HEllo this is a demo read me done in class. 1-7-21
--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+// const axios = require("axios");
+const generate = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+// fs.writeFile("./output/README.md", "This is the generated readme", (err) => {
+//     console.log("All Done!")
+// })
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
